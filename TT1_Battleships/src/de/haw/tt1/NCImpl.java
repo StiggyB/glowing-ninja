271c7aa6eb84be3c7f5a07c3877a820bb897e984
StiggyB <benjamin.burchard@gmail.com>
2013-12-10 14:50:37 +0100

    Reconfigured Project Setup, added NCImpl but nyi

---
+++ TT1_Battleships/src/de/haw/tt1/NCImpl.java
@@ -0,0 +1,20 @@
+package de.haw.tt1;
+
+import de.uniba.wiai.lspi.chord.data.ID;
+import de.uniba.wiai.lspi.chord.service.NotifyCallback;
+
+public class NCImpl implements NotifyCallback {
+
+    @Override
+    public void retrieved(ID target) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void broadcast(ID source, ID target, Boolean hit) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
