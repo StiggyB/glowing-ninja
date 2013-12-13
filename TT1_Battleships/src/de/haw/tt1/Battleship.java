2669e132d1f18cafd772d1b8b8003bbc4102a0f9
StiggyB <benjamin.burchard@gmail.com>
2013-12-13 15:34:48 +0100

    Added interval calculation in checkInterval(). We can shoot, hit and fail now! :D

--- TT1_Battleships/src/de/haw/tt1/Battleship.java
+++ TT1_Battleships/src/de/haw/tt1/Battleship.java
@@ -1,11 +1,13 @@
 package de.haw.tt1;
 
+import java.math.BigInteger;
+
 import de.uniba.wiai.lspi.chord.data.ID;
 
 public class Battleship {
 
-    private int         I;
-    private int         S;
+    private int               I;
+    private int               S;
     private boolean[]         map         = null;
     private ID                predecessor = Network.getInstance()
                                                   .getPredecessorID();
@@ -20,8 +22,9 @@ public class Battleship {
      * @param i
      * @param s
      */
-    private Battleship() {}
-    
+    private Battleship() {
+    }
+
     public static Battleship getInstance() {
         return game;
     }
@@ -53,25 +56,39 @@ public class Battleship {
     /**
      * Should calculate the intervals in our DHT space
      */
-    private void checkInterval(ID id) {
-//        for (int i = 0; i < I - 1; i++) {
-//        }
-        //TODO: Somehow calculate equidistant intervals for IDs
-        // like:
-        // if id isInInterval(LowerIntervalBorder, UpperIntervalBorder)
-        //      return true;
-        if (id.isInInterval(predecessor, myID)) {
-            System.out.println("could have hitted");
+    private int checkInterval(ID id) {
+        BigInteger upperIntervalBorder = null;
+        BigInteger lowerIntervalBorder = predecessor.toBigInteger();
+        BigInteger myIDInt = myID.toBigInteger();
+
+        BigInteger range = myIDInt.subtract(lowerIntervalBorder);
+        BigInteger oneStep = range.divide(BigInteger.valueOf(I));
+
+        
+        for (int i = 0; i < I - 1; i++) {
+            upperIntervalBorder = lowerIntervalBorder.add(oneStep);
+            if (id.isInInterval(ID.valueOf(lowerIntervalBorder), ID
+                    .valueOf(upperIntervalBorder))) {
+                return i;
+            }
+            lowerIntervalBorder = upperIntervalBorder;
         }
+
+        return -1;
     }
 
     /**
-     * Check if we got hit. Always returns false atm
+     * Check if we got hit.
+     * 
      * @param id
      * @return
      */
     public boolean gotHit(ID id) {
-        checkInterval(id);
+        int interval = checkInterval(id); 
+        if (interval != -1 && map[interval]) {
+            map[interval] = false; // ship sunk
+            return true;
+        }
         return false;
     }
 
